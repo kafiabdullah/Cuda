25345bd35c092fc791ad7d2dbef39f93d07c4296
unknown <abdullahkafi996@gmail.com>
2021-07-02 21:05:08 +0600

    add hamburger menu

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -14,3 +14,10 @@ $ (document).ready(function () {
     // for MIXER
     var mixer = mixitup('.container');
 });
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
